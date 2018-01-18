ae880cb7b3f7de8027134eac2179723866b9c282
Daniel Carrillo Herrera <danielcarrillohcse@gmail.com>
2018-01-18 11:51:56 -0500

    Solucionado el problema del template y sus datos.

--- course/views.py
+++ course/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from course.models import  Student
 # Create your views here.
-def list_student(request, pk):
+#def list_student(request, pk):
+def list_student(request):
     student = Student.objects.all()
     return render(request, template_name='list_student.html', context={'student':student})
