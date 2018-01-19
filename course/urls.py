bbe84affbdf5d1dbc22efbea84461fea0cc397a7
Daniel Carrillo Herrera <danielcarrillohcse@gmail.com>
2018-01-18 23:10:27 -0500

    Se agrego el formulario para crear un nuevo estudiante.

--- course/urls.py
+++ course/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from course import views
 
+app_name="course"
+
 urlpatterns = [
     url(r'^list_student/', views.list_student, name='list_student'),
-
+    url(r'^create_student/', views.StudentCreate.as_view(), name="create_student")
 ]
--- course/views.py
+++ course/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
-from course.models import  Student
+from django.urls import reverse
+
 # Create your views here.
+from django.views.generic.edit import CreateView
+from course.forms import StudentForm
+from django.views.generic.list import ListView
+
+from course.models import  Student
+
 #def list_student(request, pk):
 def list_student(request):
     student = Student.objects.all()
     return render(request, template_name='list_student.html', context={'student':student})
+
+
+class StudentCreate(CreateView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'add_student.html'
+
+    def get_success_url(self):
+        return reverse('course:list_student')
